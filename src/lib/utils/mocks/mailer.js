1f6b86f1df31058a5b8ef9548b3a46d204d38662
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2021-03-13 13:10:45 +0000

    REFACTORED MAILER UNIT TESTS.

--- src/lib/utils/mocks/mailer.js
+++ src/lib/utils/mocks/mailer.js
@@ -3,21 +3,20 @@
 /**
  * Mock implementation for various mailer service methods
  */
-const mockMailerImplementation =  {
-    _transporter: {
-        async sendMail(data) {
-            mockMailerImplementation.calledMethods.sendMail = true;
-            return {messageId: "fake-id", messagePreviewURL: "http://who-cares.io"}
-        }
-    },
-    _mailLib: {
-        getTestMessageUrl() {
-            mockMailerImplementation.calledMethods.getTestMessageUrl = true;
-        }
-    },
-    calledMethods: {
-        sendMail: false,
-        getTestMessageUrl: false
+ function mockMailerImplementation({eventEmitter}) {
+    eventEmitter.on("userService.newUserCreated", send)
+    const calledMethods = {
+        send: false
+    }
+    
+    async function send(data) {
+        calledMethods.send = true;
+        return {messageId: "fake-id", messagePreviewURL: "http://who-cares.io"}
+    }
+    
+    return {
+        send,
+        calledMethods
     }
 }
 
