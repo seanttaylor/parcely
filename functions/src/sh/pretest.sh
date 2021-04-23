7f37b2439829cb00c81d0a3f1cc954aef35c0d5c
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2021-04-23 17:59:21 -0400

    REMOVED ./sh FOLDER Updated Github Actions config

--- functions/src/sh/pretest.sh
+++
@@ -1,3 +0,0 @@
-#Run solely to keep thet firebase-functions package from producing a warning about missing environment variables when running tests locally via `npm test`
-export GCLOUD_PROJECT=fakeProjectId
-export FIREBASE_CONFIG=\''{"projectId": "fakeProjectId", "storageBucket": fakeProjectId.appspot.com}'\'
