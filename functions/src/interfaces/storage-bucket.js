7239a4a07b7fb459a4d1f787b45597e13ef1d88d
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2021-05-16 12:50:49 -0400

    FIXES ISSUE NO. 250 (#255)

---
+++ functions/src/interfaces/storage-bucket.js
@@ -0,0 +1,66 @@
+/* istanbul ignore file */
+
+/**
+* An object having the IStorageBucket API; a set of methods for managing storage bucktes
+* @typedef {Object} IStorageBucketAPI
+* @property {Function} create - creates a new storage bucket
+* @property {Function} listBuckets - list all buckets
+* @property {Function} listBucketContents - list all items in a specified bucket
+* @property {Function} getBucketItem - get specified bucket item
+* @property {Function} deleteBucketItem - delete specified bucket item
+* @property {Function} deleteBucket - delete a specified bucket
+* @property {Function} getBucket - get a specified bucket
+* @property {Function} putBucket - add a new entry to a specified bucket
+
+/**
+ * Interface for a storage bucket
+ * @param {IStorageBucketAPI} myImpl - object defining concrete implementations for interface methods
+ */
+
+function IStorageBucket(myImpl) {
+  function required() {
+    throw Error('Missing implementation');
+  }
+
+  /**
+     @returns {Object} - a new storage bucket
+    */
+  this.create = myImpl.create || required;
+
+  /**
+     @returns {Array} - a list of buckets
+    */
+  this.listBuckets = myImpl.listBuckets || required;
+
+  /**
+     @returns {Array} - a list of all items in the bucket
+    */
+  this.listBucketContents = myImpl.listBucketContents || required;
+
+  /**
+     @returns {Object} - a bucket item
+    */
+  this.getBucketItem = myImpl.getBucketItem || required;
+
+  /**
+     @returns
+    */
+  this.deleteBucketItem = myImpl.deleteBucketItem || required;
+
+  /**
+     @returns
+    */
+  this.deleteBucket = myImpl.deleteBucket || required;
+
+  /**
+     @returns {Object} a storage bucket
+    */
+  this.getBucket = myImpl.getBucket || required;
+
+  /**
+     @returns
+    */
+  this.putBucket = myImpl.putBucket || required;
+}
+
+module.exports = IStorageBucket;
