6c569dd89ca7cf92a17a1bc02cf712b9b187c626
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2021-05-17 16:52:25 -0400

    ADDED AUTHORIZATION LOGIC TO STORAGE API

--- functions/tests/integration/storage-service.api.test.js
+++ functions/tests/integration/storage-service.api.test.js
@@ -14,20 +14,41 @@ const superSecretPassword = 'superSecretPassword';
 
 describe('StorageAPI', () => {
   test('Should be able to get a list of storage buckets', async () => {
-    const res1 = await request.get('/storage/buckets')
+    const res1 = await request.post('/api/v1/users/token')
+    .send({
+        emailAddress: furyEmailAddress,
+        password: superSecretPassword,
+    })
+    .expect(201);
+    
+    const furyAccessToken = res1.body.accessToken;
+
+    const res2 = await request.get('/storage/buckets')
+    .set('authorization', `Bearer ${furyAccessToken}`)
     .send()
     .expect(200);
 
-    expect(Array.isArray(res1.body.entries)).toBe(true);
+    expect(Array.isArray(res2.body.entries)).toBe(true);
   });
 
   test('Should be able to get a specified storage bucket', async () => {
     const bucketId = 'crate-qr-codes';
-    const res1 = await request.get(`/storage/buckets/${bucketId}`)
+
+    const res1 = await request.post('/api/v1/users/token')
+    .send({
+        emailAddress: furyEmailAddress,
+        password: superSecretPassword,
+    })
+    .expect(201);
+    
+    const furyAccessToken = res1.body.accessToken;
+
+    const res2 = await request.get(`/storage/buckets/${bucketId}`)
+    .set('authorization', `Bearer ${furyAccessToken}`)
     .send()
     .expect(200);
 
-    expect(Array.isArray(res1.body.entries)).toBe(true);
+    expect(Array.isArray(res2.body.entries)).toBe(true);
   });
 
   test('Should be able to get a specified storage bucket item', async () => {
@@ -42,7 +63,7 @@ describe('StorageAPI', () => {
     
     const furyAccessToken = res1.body.accessToken;
 
-    const res2 = await request.post('/api/v1/crates')
+    await request.post('/api/v1/crates')
     .set('authorization', `Bearer ${furyAccessToken}`)
     .send({
         size: ['L'],
@@ -50,14 +71,58 @@ describe('StorageAPI', () => {
     })
     .expect(201);
 
-    const res3 = await request.get(`/storage/buckets/${bucketId}`)
+    const res2 = await request.get(`/storage/buckets/${bucketId}`)
+    .set('authorization', `Bearer ${furyAccessToken}`)
     .send()
     .expect(200);
 
-    const [bucketItemId] = res3.body.entries;
+    const [bucketItemId] = res2.body.entries;
 
     await request.get(`/storage/buckets/${bucketId}/${bucketItemId}`)
     .send()
     .expect(200);
+  });
+
+  test('Non-admin users should NOT be able to storage bucket resources ', async () => {
+       
+    await request.get('/storage/buckets')
+    .send()
+    .expect(401);
+  });
+
+  test('Should receive 404 status when requesting a non-existent bucket item', async () => {
+    const bucketId = 'crate-qr-codes';
+
+    const res1 = await request.post('/api/v1/users/token')
+    .send({
+        emailAddress: furyEmailAddress,
+        password: superSecretPassword,
+    })
+    .expect(201);
+    
+    const furyAccessToken = res1.body.accessToken;
+
+    const res2 = await request.get(`/storage/buckets/crate-qr-codes/dontexist`)
+    .set('authorization', `Bearer ${furyAccessToken}`)
+    .send()
+    .expect(404);
+  });
+
+  test('Should receive 404 status when requesting a non-existent bucket', async () => {
+    const bucketId = 'crate-qr-codes';
+
+    const res1 = await request.post('/api/v1/users/token')
+    .send({
+        emailAddress: furyEmailAddress,
+        password: superSecretPassword,
+    })
+    .expect(201);
+    
+    const furyAccessToken = res1.body.accessToken;
+
+    const res2 = await request.get(`/storage/buckets/dontexist`)
+    .set('authorization', `Bearer ${furyAccessToken}`)
+    .send()
+    .expect(404);
   });
 });
